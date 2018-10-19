c0a460a187d94b9c22f9916b7e14dc713a86ec93
Leo Tejeda <leonelhtejeda@gmail.com>
2018-10-19 11:52:29 -0700

    bind in the wild

--- this.js
+++ this.js
@@ -144,3 +144,27 @@ anaCalc();// Ana just calculated to 10
 
 let anaCalc2 = leo.addNumbers.bind(ana,1,2)// function(){}
 anaCalc2();// Ana just calculated to 10
+
+
+//Bind in the wild
+let colt = {
+    firstName: "Colt",
+    sayHi: function(){
+        setTimeout(function(){
+            console.log("Hi " + this.firstName)
+        },1000)
+    }
+}
+colt.sayHi();// Hi undefined (1000 milleconds later)
+
+//use bind to set the correct context of this
+
+let colt = {
+    firstName: "Colt",
+    sayHi: function(){
+        setTimeout(function(){
+            console.log("Hi " + this.firstName)
+        }.bind(this),1000)
+    }
+}
+colt.sayHi()// Hi colt (1000 milliseconds later)
